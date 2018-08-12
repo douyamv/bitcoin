f952d87564e9590386aade4d9c869a0c27951978
zhongshu.duzhongsh <zhongshu.duzhongsh@alibaba-inc.com>
2018-08-12 23:46:07 +0800

    init get address by string

---
+++ getAddress.py
@@ -0,0 +1,9 @@
+
+# -*- coding: cp936 -*-
+import sys
+from bitcoin import *
+myBrainWalletPassword = sys.argv[1]
+mySecretKey = encode_privkey(sha256(myBrainWalletPassword), "wif")
+myAddress = pubtoaddr(privtopub(mySecretKey))
+print('secretKey : ',mySecretKey)
+print('  address : ',myAddress)
